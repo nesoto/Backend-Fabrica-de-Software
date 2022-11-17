c2afa2f03a114a473dca406f6fc1334bcee3b588
Thvich <vherrera2018@udec.cl>
2022-11-16 23:41:56 -0300

    magia

--- FabricaDeSoftware/cl/src/Router.py
+++ FabricaDeSoftware/cl/src/Router.py
@@ -2,5 +2,6 @@
 
 
 GET_MAIN_API_LANDING = "/"
-GET_AVAILABLE_SPACES = "/parking/{cars_parked}"
+GET_AVAILABLE_SPACES = "/parking"
 PUT_TOTAL_SPACES = "/parking/{total_spaces}"
+POST_USED_SPACES = "/parking"
--- FabricaDeSoftware/cl/src/main.py
+++ FabricaDeSoftware/cl/src/main.py
@@ -1,12 +1,25 @@
 from typing import Union
+
+import uvicorn
 from fastapi import FastAPI
+from pydantic import BaseModel
+
 
 import Router
 
 app = FastAPI()
 
+class Item(BaseModel):
+    cars: int
+
 global TOTAL_PARKING_SPOTS
 TOTAL_PARKING_SPOTS = 50
+
+global AVAILABLE_SPOTS
+AVAILABLE_SPOTS = TOTAL_PARKING_SPOTS
+
+global USED_SPOTS
+USED_SPOTS = 0
 
 @app.get(Router.GET_MAIN_API_LANDING)
 def ping():
@@ -14,8 +27,9 @@ def ping():
 
 
 @app.get(Router.GET_AVAILABLE_SPACES)
-def available_spaces(cars_parked: int):
-    return {"cars_parked": cars_parked, "available_spaces": TOTAL_PARKING_SPOTS - cars_parked}
+def available_spaces():
+    global USED_SPOTS
+    return {"cars_parked": USED_SPOTS, "available_spaces": TOTAL_PARKING_SPOTS - USED_SPOTS}
 
 
 @app.put(Router.PUT_TOTAL_SPACES)
@@ -23,3 +37,17 @@ def update_total_spaces(total_spaces: int):
     global TOTAL_PARKING_SPOTS
     TOTAL_PARKING_SPOTS = total_spaces
     return {"total_spaces": TOTAL_PARKING_SPOTS}
+
+@app.post(Router.POST_USED_SPACES)
+async def Used_Spots(item: Item):
+    global USED_SPOTS
+    USED_SPOTS = item.cars
+    return item
+#def update_available_spaces(used_spots: int):
+#    global AVAILABLE_SPOTS
+#    AVAILABLE_SPOTS = TOTAL_PARKING_SPOTS - used_spots
+#    return {"available_spots": AVAILABLE_SPOTS}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="127.0.0.1", port=8000)
